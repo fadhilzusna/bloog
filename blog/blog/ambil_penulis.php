3711a6c8997b5baa5c302d5ae25359bf7c45c273
fadhilzusna <fadhilzusna61@gmail.com>
2026-05-04 22:52:40 +0700

    Create ambil_penulis.php

---
+++ blog/blog/ambil_penulis.php
@@ -0,0 +1,13 @@
+<?php
+include "koneksi.php";
+
+$result = $conn->query("SELECT * FROM penulis");
+
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+?>
