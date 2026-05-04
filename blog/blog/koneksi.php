a438ec90393b38c37b0baadce883ef4e76178a96
fadhilzusna <fadhilzusna61@gmail.com>
2026-05-04 23:03:55 +0700

    Create koneksi.php

---
+++ blog/blog/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "081216";
+$db   = "db_blog";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
